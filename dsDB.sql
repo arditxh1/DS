c3dc731b7901349c58c27100dea9ee734b13f5f2
arditxh1 <ardit.xhaferi@hotmail.com>
2019-10-09 16:19:54 +0200

    Version 2.0
    Ready for testing.

--- dsDB.sql
+++ dsDB.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2019 at 05:42 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.1.32
+-- Generation Time: Oct 09, 2019 at 04:19 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -41,13 +41,6 @@ CREATE TABLE `code_projekte` (
   `approved` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `code_projekte`
---
-
-INSERT INTO `code_projekte` (`id`, `Emri`, `Short`, `Link`, `username`, `user_id`, `type`, `Review`, `badges`, `approved`) VALUES
-(7, 'Egg Man', 'Its about an egg man.', 'https://studio.code.org/projects/gamelab/kSGLMkZvZX1mvyyVvJT5qKigdWOentqvWE5RS0CjJQk', 'Arditxh3', 53, 'code', 11, 'none', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -65,26 +58,6 @@ CREATE TABLE `comments` (
   `receiver_id` int(255) NOT NULL,
   `checked` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `idOfPr`, `TypeOfPr`, `Mesage`, `user_id`, `time`, `username`, `receiver_id`, `checked`) VALUES
-(209, 8, 'scratch', 'Keq e ki bo', 8, 'September 30 at 16:32', 'arditxh2', 53, 1),
-(210, 8, 'scratch', 'Nice bug', 8, 'September 30 at 16:32', 'arditxh2', 53, 1),
-(211, 8, 'scratch', 'wow', 8, 'September 30 at 16:36', 'arditxh2', 53, 1),
-(212, 8, 'scratch', 'Pse spo vjen toast', 8, 'September 30 at 16:41', 'arditxh2', 53, 1),
-(213, 8, 'scratch', 'Nice', 8, 'September 30 at 16:43', 'arditxh2', 53, 1),
-(214, 8, 'scratch', 'nice', 8, 'September 30 at 16:43', 'arditxh2', 53, 1),
-(215, 8, 'scratch', 'Nice', 8, 'September 30 at 16:43', 'arditxh2', 53, 1),
-(216, 8, 'scratch', 'Nice', 8, 'September 30 at 16:43', 'arditxh2', 53, 1),
-(217, 8, 'scratch', 'nice', 8, 'September 30 at 16:43', 'arditxh2', 53, 1),
-(218, 8, 'scratch', 'Nice', 8, 'September 30 at 17:5', 'arditxh2', 53, 1),
-(219, 8, 'scratch', 'wow', 8, 'September 30 at 17:5', 'arditxh2', 53, 1),
-(220, 8, 'scratch', 'nice', 8, 'September 30 at 17:5', 'arditxh2', 53, 1),
-(221, 8, 'scratch', 'Nice', 8, 'September 30 at 17:7', 'arditxh2', 53, 1),
-(222, 8, 'scratch', 'Nicer', 8, 'September 30 at 17:7', 'arditxh2', 53, 1);
 
 -- --------------------------------------------------------
 
@@ -105,13 +78,6 @@ CREATE TABLE `kodu_projekte` (
   `badges` varchar(50) NOT NULL DEFAULT 'none',
   `approved` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `kodu_projekte`
---
-
-INSERT INTO `kodu_projekte` (`id`, `Emri`, `Short`, `File`, `Link`, `user_id`, `username`, `type`, `Review`, `badges`, `approved`) VALUES
-(10, 'Oktapode', 'Kjo loje nevoitet per qe oktapodet te hane dy peshq kurse peshqit e medhenje ne ', 'uploads/5d921eb343df15.19789367.kodu', '', 53, 'arditxh3', 'kodu', 11, 'none', 0);
 
 -- --------------------------------------------------------
 
@@ -171,13 +137,6 @@ CREATE TABLE `scratch_projekte` (
   `approved` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `scratch_projekte`
---
-
-INSERT INTO `scratch_projekte` (`id`, `Emri`, `Short`, `Link`, `username`, `user_id`, `type`, `Review`, `badges`, `approved`) VALUES
-(8, 'Plantera', 'Select a type of plant then place the plant of your choice on a location.', 'https://scratch.mit.edu/projects/322001543/', 'Arditxh3', 53, 'scratch', 11, 'idea,code,design', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -198,13 +157,6 @@ CREATE TABLE `stencyl_projekte` (
   `approved` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `stencyl_projekte`
---
-
-INSERT INTO `stencyl_projekte` (`id`, `Emri`, `Short`, `SCR`, `File`, `user_id`, `username`, `type`, `Review`, `badges`, `approved`) VALUES
-(4, 'Enemy Follower and Enemy', 'Enemy Follower and Enemy enemy Follower and Enemy.', 'uploads/5d9220094671c9.35148578.png', 'uploads/5d9220093f7aa4.25876570.stencyl', 53, 'arditxh3', 'stencyl', 11, 'none', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -224,8 +176,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `type`) VALUES
-(8, 'Arditxh2', 'ardit.xhafer@hotmail', '$2y$10$nI6Xlst3FuTjUjz9MgPRFONqF0jIuL3I0no.d1m7W7MU7GCZVOdxi', 'admin'),
-(53, 'Arditxh3', 'ardit.xhaferi@hotmai', '$2y$10$rJLgadZLtOhOgtIdycL8cOrWjX4yaN4JzLRImCISikoJd8b32iaYG', 'user');
+(8, 'Arditxh2', 'ardit.xhafer@hotmail', '$2y$10$nI6Xlst3FuTjUjz9MgPRFONqF0jIuL3I0no.d1m7W7MU7GCZVOdxi', 'admin');
 
 -- --------------------------------------------------------
 
